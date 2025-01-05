504d5f1bd8d5cf76ad81c317e2d14f44544e0dcb
joe <jahufford@gmail.com>
2025-01-04 21:06:26 -0700

    Project framework for running google tests from regular main

---
+++ src/tests/tests.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include "../myclass.h"
+
+// int main(int arc, *argv[])
+// {
+//     ::testing::InitGoogleTest(&argc, argv);
+//     int ret{RUN_ALL_TESTS()};
+//     if(!ret){
+//         std::cout << "<<<SUCCESS>>>" << std::endl;
+//     }else{
+//         std::cout << "FAILED" << std::endl;
+//     }
+// }
+
+TEST(someTest,testTwo)
+{
+  ASSERT_EQ(5,5);
+}
+
+TEST(moreTests,testOne)
+{
+    MyClass mc;
+    ASSERT_NE(mc.add(3,5),9);
+    ASSERT_EQ(mc.add(-5,4),-1);
+}
+TEST(moreTests,testTwizzy)
+{
+    MyClass mc;
+    ASSERT_NE(mc.add(3,5),9);
+    ASSERT_EQ(mc.add(-5,4),-1);
+}
